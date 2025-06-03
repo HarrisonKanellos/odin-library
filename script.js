c8e58a0f9ceb03fdfffe4ef566e892e012755f1c
HarrisonKanellos <harrisonkanellos@gmail.com>
2025-06-03 16:32:03 +1000

    Add ownedStatus attribute to Book constructor

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, image, readStatus) {
+function Book(title, author, image, ownedStatus, readStatus) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.image = image;
+    this.ownedStatus = ownedStatus;
     this.readStatus = readStatus;
 }
 
 // Creates a new book, then adds to myLibrary array
-function addBookToLibrary(title, author, image, readStatus) {
-    let book = new Book(title, author, image, readStatus);
+function addBookToLibrary(title, author, image, ownedStatus, readStatus) {
+    let book = new Book(title, author, image, ownedStatus, readStatus);
     myLibrary.push(book);
 }
-
